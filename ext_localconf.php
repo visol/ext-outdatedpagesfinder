06f4a6545e4ac259343feef99925c74c75308f06
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2015-03-23 16:33:40 +0100

    [TASK] Initial commit with JavaScript from TYPO3 Core

---
+++ ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+if (TYPO3_MODE === 'BE') {
+//	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerExtDirectComponent('TYPO3.Components.PageTree.DataProvider', 'Visol\\Outdatedpagesfinder\\Tree\\Pagetree\\ExtdirectTreeDataProvider', 'web', 'user,group');
+}
