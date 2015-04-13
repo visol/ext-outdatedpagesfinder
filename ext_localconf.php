c594cb15513ce43a11fb0e7e635cc7abbca475ee
lorenzulrich <lulrich@hispeed.ch>
2015-04-13 16:34:35 +0200

    [TASK] Cleanup

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,10 +2,6 @@
 
 defined('TYPO3_MODE') or die();
 
-//if (TYPO3_MODE === 'BE') {
-//	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerExtDirectComponent('TYPO3.Components.PageTree.DataProvider', 'Visol\\Outdatedpagesfinder\\Tree\\Pagetree\\ExtdirectTreeDataProvider', 'web', 'user,group');
-//}
-
 // add own backend items
 $GLOBALS['TYPO3_CONF_VARS']['typo3/backend.php']['additionalBackendItems'][] =
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('outdatedpagesfinder', 'backend_ext.php');
--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,3 @@
 <?php
 
 defined('TYPO3_MODE') or die();
-
-//if (TYPO3_MODE === 'BE') {
-//	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addNavigationComponent('web', 'typo3-pagetree', 'outdatedpagesfinder');
-//}
