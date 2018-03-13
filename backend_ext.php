1ae654046ec378bab5e5e97d63496843d5458534
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2018-03-13 20:34:57 +0000

    [TASK] Adjust to TYPO3 8.7

--- backend_ext.php
+++ backend_ext.php
@@ -3,9 +3,8 @@
 if (is_object($TYPO3backend)) {
     /** @var \TYPO3\CMS\Backend\Template\DocumentTemplate $template */
     $template = $GLOBALS['TBE_TEMPLATE'];
-    $pageRenderer = $template->getPageRenderer();
+    $pageRenderer =\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
     $path = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('outdatedpagesfinder');
     $pageRenderer->addJsFile($path . 'Resources/Public/Javascript/PagetreeFilterExtension.js');
-
     $pageRenderer->addCssFile($path . 'Resources/Public/StyleSheets/PagetreeFilterStyles.css');
 }
